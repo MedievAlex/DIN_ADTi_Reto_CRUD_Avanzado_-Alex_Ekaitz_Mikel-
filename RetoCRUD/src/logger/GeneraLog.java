3d2c64a3ba9a2611bdf151d20926c108c54b70ed
Ekasestao <ekaitzcampo2002@gmail.com>
2026-01-23 13:09:04 +0100

    Add logger

---
+++ RetoCRUD/src/logger/GeneraLog.java
@@ -0,0 +1,69 @@
+package logger;
+
+import java.io.IOException;
+import java.io.File;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class GeneraLog
+{
+	// [ VARIABLES ]
+  private static Logger logger = null;
+  private static FileHandler fileHandler = null;
+
+  // [ CONSTRUCTORES ]
+  // Constructor privado para evitar la instanciación externa
+	private GeneraLog()
+	{
+		// Inicializa el logger solo si no se ha inicializado previamente
+		if (logger == null)
+		{
+			File carpeta = new File("./logs"); // Ruta de la carpeta
+
+			if (!carpeta.exists())
+			{
+				carpeta.mkdir();
+			}
+
+			try
+			{
+				// Configura el logger
+				logger = Logger.getLogger(GeneraLog.class.getName());
+				// A false evita la salida en consola
+				logger.setUseParentHandlers(false);
+				fileHandler = new FileHandler("./logs/ProjectLogs.log", true); // Ruta de la carpeta + nombre archivo .log [true = append mode]
+
+				SimpleFormatter formatter = new SimpleFormatter();
+				fileHandler.setFormatter(formatter);
+				logger.addHandler(fileHandler);
+				logger.setLevel(Level.ALL); // Establece el nivel de log (puedes ajustarlo según tu necesidad)
+			}
+			catch (IOException e)
+			{
+				System.err.println("[ERROR AL CONFIGURAR EL LOGGER]: " + e.getMessage());
+			}
+		}
+	}
+
+	// [ METODOS ]
+	// Método para obtener la instancia del logger
+	public static Logger getLogger()
+	{
+		if (logger == null)
+		{
+			new GeneraLog(); // Si aún no se ha creado, inicialízalo
+		}
+		return logger;
+	}
+
+	// Método para cerrar el fileHandler
+	public static void closeLogger()
+	{
+		if (fileHandler != null)
+		{
+			fileHandler.close();
+		}
+	}
+}
