70d4f4c4ba6b345f2aa153ff57002a30be69f469
MedievAlex <alexirazola@gmail.com>
2026-01-28 13:40:25 +0100

    Pierdo más tiempo pensando COMO HACER EL TEST que debugeando. Que cosa más inutil.

--- RetoCRUD/src/dao/MockClassDAO.java
+++ RetoCRUD/src/dao/MockClassDAO.java
@@ -56,6 +56,8 @@ public class MockClassDAO implements ClassDAO
         testUserLists.put("Favorites", new ArrayList<>());
         userListGames.put("testuser", testUserLists);
 
+        
+        
         this.shouldThrowException = false;
     }
 
--- RetoCRUD/test/viewText/ListTest.java
+++ RetoCRUD/test/viewText/ListTest.java
@@ -3,10 +3,9 @@ package viewText;
 import controller.Controller;
 import controller.ListWindowController;
 import dao.MockClassDAO;
-import java.time.LocalDate;
+import javafx.collections.ObservableList;
 import model.Profile;
 import model.User;
-import model.Review;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.FixMethodOrder;
@@ -16,15 +15,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import model.Pegi;
-import model.Platform;
-import model.VideoGame;
 
 import static org.junit.Assert.*;
 
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@FixMethodOrder(MethodSorters.DEFAULT)
 public class ListTest extends ApplicationTest {
 
     private ListWindowController controller;
@@ -37,8 +35,8 @@ public class ListTest extends ApplicationTest {
         mockDAO = new MockClassDAO();
         realController = new Controller(mockDAO);
         
-        mockUser = new User("MALE", "ES9876543210987654321098", "asanchez", "Ab123456",
-                "admin@example.com", "Admin", "987654321", "User");
+        this.mockUser = new User("MALE", "ES1234567890123456789012", "testuser", "Ab123456",
+                "test@test.com", "Test", "123456789", "User");
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/ListWindow.fxml"));
         Parent root = loader.load();
@@ -55,29 +53,15 @@ public class ListTest extends ApplicationTest {
 
     @Before
     public void setUp() {
-        interact(() -> {
-            VideoGame videoGame = new VideoGame("DEFAULT_GAME", LocalDate.now(), Platform.DEFAULT, Pegi.DEFAULT);
-            videoGame.setV_id(1);
-            mockDAO.addMockVideoGame(videoGame);
-            videoGame = new VideoGame("Owlboy", LocalDate.of(2016, 11, 1), Platform.NINTENDO, Pegi.PEGI3);
-            videoGame.setV_id(2);
-            mockDAO.addMockVideoGame(videoGame);
-            videoGame = new VideoGame("Animal Crossing New Horizons", LocalDate.of(2020, 5, 20), Platform.NINTENDO, Pegi.PEGI6);
-            videoGame.setV_id(3);
-            mockDAO.addMockVideoGame(videoGame);
-            videoGame = new VideoGame("Detroit: Become Human", LocalDate.of(2018, 5, 25), Platform.PLAYSTATION, Pegi.PEGI16);
-            videoGame.setV_id(4);
-            mockDAO.addMockVideoGame(videoGame);         
-        });
+        mockDAO.setShouldThrowException(false, null);
     }
 
     @Test
     public void test1_AllComponentsAreLoaded() {
         MenuButton menuButton = lookup("#menu").query();
         Text listName = lookup("#listName").query();  
-        
         TableView<?> tableLists = lookup("#tableLists").query();
-        
+        VBox vbLists = lookup("#vbLists").query();
         Button bttnRemove = lookup("#bttnRemove").query();
         Button bttnAdd = lookup("#bttnAdd").query();
         ComboBox<String> combLists = lookup("#combLists").query();
@@ -85,6 +69,8 @@ public class ListTest extends ApplicationTest {
         assertNotNull(menuButton);
         assertNotNull(listName);
         assertNotNull(tableLists);
+        assertNotNull(vbLists);
+        assertNotNull(vbLists.getChildren());
         assertNotNull(bttnRemove);
         assertNotNull(bttnAdd);
         assertNotNull(combLists);
@@ -110,17 +96,57 @@ public class ListTest extends ApplicationTest {
     }
 
     @Test
-    public void test3_TableReviewsLoaded() {
+    public void test3_TableListsLoaded() {
         TableView<?> tableLists = lookup("#tableLists").query();
         sleep(1000);
 
         assertNotNull(tableLists);
         assertTrue(tableLists.getItems().size() >= 0);
     }
-
-    /*
+    
     @Test
-    public void test5_ComboBoxListsLoaded() {
+    public void test4_NewListButton() {
+        VBox vbLists = lookup("#vbLists").query();
+        ObservableList listButtons = vbLists.getChildren();
+        listButtons.get(1);
+        // ???
+    }
+
+    @Test
+    public void test5_ListButtonsLoad() {
+        //Clickar a un boton y que la tabla cambie de valores
+    }
+
+    @Test
+    public void test6_RenameListName() {
+        // Hacer click derecho y que se pueda abrir la ventana para renombrar
+    }
+
+    @Test
+    public void test7_DeleteList() {
+        // Hacer click derecho y que se pueda eliminar la lista
+    }
+    
+    @Test
+    public void test8_SelectGameInTable() {
+        TableView<?> tableLists = lookup("#tableLists").query();
+        sleep(1000);
+        
+        if (tableLists.getItems().size() > 0) {
+            clickOn(".table-row-cell");
+            sleep(500);
+        }
+    }
+    
+    @Test
+    public void test9_RemoveGames() {
+        // Que al darle a remove se elimine el juego
+        Button bttnRemove = lookup("#bttnRemove").query();
+    }
+    
+    @Test
+    public void test10_ComboBoxListsLoaded() {
+        // Que se puedan seleccionar juegos con el combobox
         ComboBox<String> combLists = lookup("#combLists").query();
         sleep(500);
 
@@ -129,93 +155,15 @@ public class ListTest extends ApplicationTest {
 
         interact(() -> {
             assertTrue(combLists.getItems().size() > 0);
-            assertEquals("All Reviews", combLists.getItems().get(0));
+            assertEquals("Favorites", combLists.getItems().get(0));
         });
         push(KeyCode.ESCAPE);
         sleep(500);
     }
-
+    
     @Test
-    public void test6_SelectReviewInTable() {
-        TableView<Review> tableReview = lookup("#tableReview").query();
-        sleep(1000);
-
-        if (tableReview.getItems().size() > 0) {
-            clickOn(".table-row-cell");
-            sleep(500);
-            assertNotNull(tableReview.getSelectionModel().getSelectedItem());
-        }
+    public void test11_AddGamesGames() {
+        // Que al darle a add se añada el juego
+        Button bttnAdd = lookup("#bttnAdd").query();
     }
-
-    @Test
-    public void test7_NewReviewButton() {
-        Button btnNewReview = lookup("#btnNewReview").query();
-        assertNotNull(btnNewReview);
-
-        clickOn("#btnNewReview");
-        sleep(500);
-        assertNotNull(controller);
-    }
-
-    @Test
-    public void test8_FilterByListSelection() {
-        ComboBox<String> combLists = lookup("#combLists").query();
-        sleep(500);
-        clickOn("#combLists");
-        sleep(500);
-
-        interact(() -> {
-            if (combLists.getItems().size() > 0) {
-                combLists.getSelectionModel().select(0);
-                //controller.showReviewsByList();
-            }
-        });
-
-        sleep(500);
-
-        TableView<Review> tableReview = lookup("#tableReview").query();
-        assertNotNull("Table should not be null after filtering", tableReview);
-    }
-
-    @Test
-    public void test9_BorrarPrimeraReviewDirecto() {
-        Profile admin = new User("MALE", "ES9876543210987654321098","asanchez", "Ab123456", "admin@example.com", "Admin", "987654321", "User");
-        interact(() -> {
-            controller.setUsuario(admin);
-        });
-        sleep(1000);
-        TableView<Review> tabla = lookup("#tableReview").query();
-        if (tabla.getItems().isEmpty()) {
-            interact(() -> {
-                try {
-                    Profile autor = new User("MALE", "ES1111111111111111111111","autorprueba", "Ab123456", "autor@test.com", "Autor", "111111111", "Prueba");
-                    VideoGame juego = new VideoGame("Juego para Borrar",LocalDate.now(), Platform.PC, Pegi.PEGI12);
-                    juego.setV_id(999);
-                    Review review = new Review(autor, juego, 7,"Esta review será borrada", LocalDate.now(), Platform.PC);
-
-                    mockDAO.addMockVideoGame(juego);
-                    mockDAO.addMockReview(review);
-                    //controller.loadReview(); 
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            });
-            sleep(1000);
-        }
-        tabla = lookup("#tableReview").query();
-        int totalInicial = tabla.getItems().size();
-        rightClickOn(tabla);
-        sleep(500);
-
-        try {
-            clickOn("Delete Review");
-            sleep(1000);
-            TableView<Review>tablaFinal = lookup("#tableReview").query();
-            int totalFinal = tablaFinal.getItems().size();
-            assertTrue(totalFinal < totalInicial);
-        } catch (Exception e) {
-            push(KeyCode.ESCAPE);
-        }
-    }
-*/
 }
